4d313424145c7c8701eab8713e9bda8738dd040a
cmrohityadav <cmrohityadav23@gmail.com>
2025-05-20 00:08:25 +0530

    added followed srp

---
+++ 04_SOLID/02_followedSRP.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Product class representing any item in eCommerce.
+class Product {
+public:
+    string sName;
+    double dPrice;
+
+    Product(string name, double price) {
+        this->sName = name;
+        this->dPrice = price;
+    }
+};
+
+//1. ShoppingCart: Only responsible for Cart related business logic.
+class ShoppingCart {
+private:
+    vector<Product*> products; // Store heap-allocated products
+
+public:
+    void addProduct(Product* p) { 
+        products.push_back(p);
+    }
+
+    const vector<Product*>& getProducts() { 
+        return products;
+    } 
+
+    //Calculates total price in cart.
+    double calculateTotal() {
+        double dTotal = 0;
+        for (auto p : products) {
+            dTotal += p->dPrice;
+        }
+        return dTotal;
+    }
+};
+
+// 2. ShoppingCartPrinter: Only responsible for printing invoices
+class ShoppingCartPrinter {
+private:
+    ShoppingCart* cart; 
+
+public:
+    ShoppingCartPrinter(ShoppingCart* cart) { 
+        this->cart = cart; 
+    }
+
+    void printInvoice() {
+        cout << "Shopping Cart Invoice:\n";
+        for (auto p : cart->getProducts()) {
+            cout << p->sName << " - Rs " << p->dPrice << endl;
+        }
+        cout << "Total: Rs " << cart->calculateTotal() << endl;
+    }
+};
+
+// 3. ShoppingCartStorage: Only responsible for saving cart to DB
+class ShoppingCartStorage {
+private:
+    ShoppingCart* cart; 
+
+public:
+    ShoppingCartStorage(ShoppingCart* cart) { 
+        this->cart = cart; 
+    }
+
+    void saveToDatabase() {
+        cout << "Saving shopping cart to database..." << endl;
+    }
+};
+
+int main() {
+    ShoppingCart* cart = new ShoppingCart();
+
+    cart->addProduct(new Product("Laptop", 50000));
+    cart->addProduct(new Product("Mouse", 2000));
+
+    ShoppingCartPrinter* printer = new ShoppingCartPrinter(cart);
+    printer->printInvoice();
+
+    ShoppingCartStorage* db = new ShoppingCartStorage(cart);
+    db->saveToDatabase();
+
+    return 0;
+}
